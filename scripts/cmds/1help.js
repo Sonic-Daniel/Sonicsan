77f3c2670c4c90912d0d5502fabca9b5ecf0b64e
Sonic-Daniel <danielthiam54@gmail.com>
2024-06-26 03:41:28 +0100

    Update 1help.js

--- scripts/cmds/1help.js
+++ scripts/cmds/1help.js
@@ -46,7 +46,7 @@ module.exports = {
 
       Object.keys(categories).forEach((category) => {
         if (category !== "info") {
-          msg += `\n⚔️ 🩸☞${category.toUpperCase()}☜🩸 ⚔️`;
+          msg += `\n⚔️ 🩸☞${category.toUpperCase()}☜🩸 ⚔️\n`;
 
 
           const names = categories[category].commands.sort();
