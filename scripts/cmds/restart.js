ea49c3b5087120dd391d7f6e6a50b2dbcd7297a2
Sonic-Daniel <danielthiam54@gmail.com>
2024-07-05 01:00:24 +0100

    Update restart.js

--- scripts/cmds/restart.js
+++ scripts/cmds/restart.js
@@ -27,7 +27,7 @@ module.exports = {
 			restartting: "🔄 | Đang khởi động lại bot..."
 		},
 		en: {
-			restartting: "🔑 | 𝑅𝑒𝑠𝑡𝑎𝑟𝑡𝑖𝑛𝑔 𝐵𝑜𝑡 (҂`_´)...□□□□□ 0%✨"
+			restartting: "🔑 | 𝑅𝑒𝑠𝑡𝑎𝑟𝑡𝑖𝑛𝑔 𝐵𝑜𝑡 (҂`_´)\n□□□□□0%✨"
 		}
 	},
 
@@ -35,7 +35,7 @@ module.exports = {
 		const pathFile = `${__dirname}/tmp/restart.txt`;
 		if (fs.existsSync(pathFile)) {
 			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
-			api.sendMessage(`🍀 | 𝐁𝐨𝐭 𝐫𝐞𝐬𝐭𝐚𝐫𝐭𝐞𝐝\■■■■ 100%(⁠｡⁠•̀⁠ᴗ⁠-⁠)⁠✧🕒 | Time: ${(Date.now() - time) / 1000}s`, tid);
+			api.sendMessage(`❤ | 𝐁𝐨𝐭 𝐫𝐞𝐬𝐭𝐚𝐫𝐭𝐞𝐝\n■■■■100%(⁠｡⁠•̀⁠ᴗ⁠-⁠)⁠✧\n🕒 | Time: ${(Date.now() - time) / 1000}s`, tid);
 			fs.unlinkSync(pathFile);
 		}
 	},
