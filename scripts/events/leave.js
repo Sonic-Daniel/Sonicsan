1d9543a363cc22c972df99060f77f4d4f1fdb53f
ʬʆʬ Sønïč ʬʆʬ <danielthiam54@gmail.com>
2024-07-09 07:58:56 +0100

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -23,9 +23,9 @@ module.exports = {
 			session2: "noon",
 			session3: "afternoon",
 			session4: "evening",
-			leaveType1: "left",
-			leaveType2: "was kicked from",
-			defaultLeaveMessage: "{userName} {type} the group"
+			leaveType1: "s'est enfin décidé de dégager",
+			leaveType2: "s'est fait expulsé",
+			defaultLeaveMessage: "☘️{userName}☘️ {type} du groupe"
 		}
 	},
 
