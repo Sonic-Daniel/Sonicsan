1bbef0ef8a632bd8c018a46566412616c869d725
ʬʆʬ Sønïč ʬʆʬ <danielthiam54@gmail.com>
2024-08-05 22:16:13 +0100

    Update sex.js

--- scripts/cmds/sex.js
+++ scripts/cmds/sex.js
@@ -1,62 +1,33 @@
 module.exports = {
-  config: {
-    name: "sex",
-    aliases: ["xxxxx"],
-    version: "1.0",
-    author: "ʬʆʬ Sønïč Shïsûį ʬɸʬ",
-    countDown: 10,
-    role: 0,
-    shortDescription: "Pornography videos ",
-    longDescription: "send you Pornography videos",
-    category: "18+",
-    guide: "{p}{n}hvdo",
-  },
+	config: {
+		name: "sex",
+		aliases: ["porno"],
+		version: "1.0",
+		author: "ʬʆʬ Sønïč Shïsûį ʬɸʬ", // this cmd will expire if you change this credits
+		countDown: 5,
+		role: 0,
+		shortDescription: "send you pic of nude",
+		longDescription: "sends u pic of girls nude",
+		category: "18+",
+		guide: "{pn}"
+	},
 
-  sentVideos: [],
-
-  onStart: async function ({ api, event, message }) {
-    const senderID = event.senderID;
-
-    const loadingMessage = await message.reply({
-      body: "🔞 | xvideos.com.",
-    });
-
-    const link = [
-"https://tinyurl.com/2y473tj5",
-"https://tinyurl.com/2d9hu58z",
-"https://tinyurl.com/2btdbbdc",
-"https://tinyurl.com/26ncj27v",
-"https://tinyurl.com/2bb65ufm",
-"https://tinyurl.com/2angjcb9",
-"https://tinyurl.com/25reoce4",
-"https://tinyurl.com/27qrule7",
-"https://tinyurl.com/2xzdrgce",
-"https://tinyurl.com/2dqc54kk",
-"https://tinyurl.com/2dlna4zb",
-"https://tinyurl.com/225douec",
-"https://tinyurl.com/22s8vtr7",
-];
-
-    const availableVideos = link.filter(video => !this.sentVideos.includes(video));
-
-    if (availableVideos.length === 0) {
-      this.sentVideos = [];
-    }
-
-    const randomIndex = Math.floor(Math.random() * availableVideos.length);
-    const randomVideo = availableVideos[randomIndex];
-
-    this.sentVideos.push(randomVideo);
-
-    if (senderID !== null) {
-      message.reply({
-        body: "❤🔞 Pornography loaded successfully 🔞❤",
-        attachment: await global.utils.getStreamFromURL(randomVideo),
-      });
-
-      setTimeout(() => {
-        api.unsendMessage(loadingMessage.messageID);
-      }, 5000);
-    }
-  },
+	onStart: async function ({ message }) {
+	 var link = [ 
+"https://tinyurl.com/2y3rejkg",
+"https://tinyurl.com/2bxgeswv",
+"https://tinyurl.com/23ovrxey",
+"https://tinyurl.com/25qrbsx3",
+"https://tinyurl.com/242q8f67",
+"https://tinyurl.com/223rwzpp",
+"https://tinyurl.com/2clxrozu",
+"https://tinyurl.com/2ccxoswo",
+"https://tinyurl.com/2cqn9h2b",
+"https://tinyurl.com/24tz3szw",
+  ]
+let img = link[Math.floor(Math.random()*link.length)]
+message.send({
+  body: '「 ❤️🔞 Pornography loaded successfully💦🥵 」',attachment: await global.utils.getStreamFromURL(img)
+})
+  } 
 }
