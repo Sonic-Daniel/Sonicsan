59eab52077fc31486ab5e8204027c6a4db30d23b
ʬʆʬ Sønïč ʬʆʬ <danielthiam54@gmail.com>
2024-08-26 01:39:15 +0100

    Update 🍂.js

--- scripts/cmds/🍂.js
+++ scripts/cmds/🍂.js
@@ -1,20 +1,41 @@
+const fs = require('fs');
+
 module.exports = {
-    config: {
-        name: "🍂",
-        version: "1.0",
-        author: "ʬʆʬ Sønïč Shïsûį ʬɸʬ", // do not change this credits
-        countDown: 5,
-        role: 0,
-        shortDescription: "No Prefix",
-        longDescription: "No Prefix",
-        category: "reply",
-    },
-onStart: async function(){}, 
-onChat: async function({
-    event,
-    message,
-    getLang
-}) {
-    if (event.body && event.body.toLowerCase() == "🍂") return message.reply("Ce n’est pas parce que tu es fort que tu pourras gagner⚪🍂🏁. Ce n’est pas parce que tu ne seras pas fort que tu perdras🐝🎶☘️");
-}
+  config: {
+    name: "🍂",
+    version: "1.0",
+    author: "Sønïč Shïsûį",
+    countDown: 5,
+    role: 0,
+    shortDescription: "",
+    longDescription: "",
+    category: "reply",
+  },
+ 
+  onStart: async function() {},
+ 
+  onChat: async function({ event, message, getLang, api }) {
+   const link = [
+"https://i.ibb.co/By078j8/image.gif",
+]
+  let img =
+link[Math.floor(Math.random()*link.length)]
+    if (event.body) {
+      const word = event.body.toLowerCase();
+      switch (word) {
+        case "🍂":
+          const replies = [
+            "📣| 𝙲𝙴 𝙽'𝙴𝚂𝚃 𝙿𝙰𝚂 𝙿𝙰𝚁𝙲𝙴 𝚀𝚄𝙴 𝚃𝚄 𝙴𝚂 𝙵𝙰𝙸𝙱𝙻𝙴 𝚀𝚄𝙴 𝚃𝚄 𝙽'𝙸𝚁𝙰𝚂 𝙿𝙰𝚂 𝙹𝚄𝚂𝚀𝚄'𝙰𝚄 𝙱𝙾𝚄𝚃 𝙳𝙴 𝚃𝙴𝚂 𝚁𝙴𝚅𝙴𝚂 !🔮",
+          ];
+          api.setMessageReaction("🔮", event.messageID, event.messageID, api); 
+          const randomIndex = Math.floor(Math.random() * replies.length);
+          message.reply({
+            body: replies[randomIndex],
+attachment: await global.utils.getStreamFromURL(img)})
+          break;
+        default:
+          return; 
+      }
+    }
+  },
 };
