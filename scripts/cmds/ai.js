25153e247d708024d17ff5ed6f7a6ae6c2f60d2a
ʬʆʬ Sønïč ʬʆʬ <danielthiam54@gmail.com>
2024-08-26 01:37:34 +0100

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -1,25 +1,41 @@
+const fs = require('fs');
+
 module.exports = {
-    config: {
-        name: "ai",
-        version: "1.0",
-        author: "ʬʆʬ Sønïč Shïsûį ʬɸʬ", // this cmd will expire if will change the credits
-        countDown: 5,
-        role: 0,
-        shortDescription: "No Prefix",
-        longDescription: "No Prefix",
-        category: "reply",
-    },
-onStart: async function(){}, 
-onChat: async function({
-    event,
-    message,
-    getLang
-}) {
-  const link = [
+  config: {
+    name: "ai",
+    version: "1.0",
+    author: "Sønïč Shïsûį", // this cmd will expire if you change this credits
+    countDown: 5,
+    role: 0,
+    shortDescription: "",
+    longDescription: "",
+    category: "reply",
+  },
+ 
+  onStart: async function() {},
+ 
+  onChat: async function({ event, message, getLang, api }) {
+   const link = [
 "https://i.ibb.co/xjN5mq3/image.jpg",
 ]
- let img = link[Math.floor(Math.random()*link.length)]
-    if (event.body && event.body.toLowerCase() == "ai") return message.send({
-  body: "Ta gueule on dit Shisui....pas ai🚫",attachment: await global.utils.getStreamFromURL(img)})
-}
+  let img =
+link[Math.floor(Math.random()*link.length)]
+    if (event.body) {
+      const word = event.body.toLowerCase();
+      switch (word) {
+        case "ai":
+          const replies = [
+            "🚫| 𝐒𝐚𝐥𝐞 𝐝𝐞𝐥𝐢𝐧𝐪𝐮𝐚𝐧𝐭 𝐚𝐩𝐩𝐞𝐥𝐥𝐞𝐬 𝐦𝐨𝐢 𝐩𝐚𝐫 𝐬𝐡𝐢𝐬𝐮𝐢....𝐩𝐚𝐬 𝐚𝐢 !",
+          ];
+          api.setMessageReaction("🖕", event.messageID, event.messageID, api); 
+          const randomIndex = Math.floor(Math.random() * replies.length);
+          message.reply({
+            body: replies[randomIndex],
+attachment: await global.utils.getStreamFromURL(img)})
+          break;
+        default:
+          return; 
+      }
+    }
+  },
 };
